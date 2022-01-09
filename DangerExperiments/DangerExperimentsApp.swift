aba6c01fb0a5eb841e43c150d2f5e699fec13cf5
Oleg Langer <oleglanger@Olegs-Air.fritz.box>
2022-01-09 19:37:23 +0100

    Add sample project

---
+++ DangerExperiments/DangerExperimentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerExperimentsApp.swift
+//  DangerExperiments
+//
+//  Created by Oleg Langer on 09.01.22.
+//
+
+import SwiftUI
+
+@main
+struct DangerExperimentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
